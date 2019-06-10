4a56cf17bfb5b655984ac3f0a913e163486f36ef
irvin lin <irvinln@gmail.com>
2019-06-10 16:59:29 +0800

    import file

--- test/index.js
+++ test/index.js
@@ -2,13 +2,15 @@ var chai = require('chai')
 var assert = chai.assert
 const mock = require('mock-fs')
 const fs = require('fs')
+var findtodos = require('../index.js')
 
-describe('mock()', function() {
-    it('configures the real fs module with a mock file system', function() {
-      mock({
-        'fake-file-for-testing-only': 'file content'
-      })
-      assert.isTrue(fs.existsSync('fake-file-for-testing-only'))
-      mock.restore()
+describe('File System', function() {
+  it('should create 2 JS files', () => {
+    findtodos()
+    mock({
+      'fake-file-for-testing-only': 'file content'
     })
+    assert.isTrue(fs.existsSync('fake-file-for-testing-only'))
+    mock.restore()
+  })
 })
