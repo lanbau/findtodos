9509c3fc23c511f926e4c6ba3e100aa84d1e0f87
irvin lin <irvinln@gmail.com>
2019-06-10 18:54:31 +0800

    Test: 3 Folders Exist

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const fs = require('fs')
 
-module.exports = (path) => {
-  var names = ['one', 'two']
-  names.forEach(name => {
-    fs.writeFile(path + '/' + name + '.js', 'TODOS', () => {
-      console.log('file created')
-    })
+module.exports = (path, callback) => {
+  fs.readdir(path, (err,files) => {
+    callback(files)
   })
 }
--- test/index.js
+++ test/index.js
@@ -1,13 +1,26 @@
 var chai = require('chai')
-var assert = chai.assert
+var expect = chai.expect
 var findtodos = require('../index.js')
 const path = require('path')
-const filePath = path.join(__dirname, '/test/')
 const fs = require('fs')
+var filePath = path.join(__dirname, '../somedir')
 
 describe('File System', function() {
 
   // My Function Does NOT need to Create Files
   // My Function will loop through directory to find TODO files
+
+  // Read Directory And Check If Folders Or Files exists
+  // If Folder Exist, Loop Internally and Check if folder or file
+  // If File, read file and check TODO, append File Path To External Array
+
+  it ('should return 3 folders', () => {
+    let result
+    findtodos(filePath, (files) => {
+      const array = ['somedir1', 'somedir2', 'somedir3']
+      expect(files).to.deep.equal(array)
+    })
+  })
+  
 
 })
