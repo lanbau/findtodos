abf23b52601c2583a74504fa5bfff0694620c518
irvin lin <irvinln@gmail.com>
2019-06-10 21:09:24 +0800

    Took hours to find out async was screwing things up

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const fs = require('fs')
 
 module.exports = (path, callback) => {
-  fs.readdir(path, (err,files) => {
-    callback(files)
+
+  var arr = []
+  checkFile(path, arr)
+  console.log(arr)
+}
+
+function checkFile (path, arr) {
+  let files = fs.readdirSync (path)
+  files.forEach(file => {
+    if (!file.includes('.js')) {
+      checkFile(path + '/' + file, arr)
+    }
+    if (file.includes('.js')) {
+      arr.push(path + '/' + file)
+    }
   })
 }
--- test/index.js
+++ test/index.js
@@ -1,26 +1,26 @@
 var chai = require('chai')
+const deepEqualInAnyOrder = require('deep-equal-in-any-order')
 var expect = chai.expect
 var findtodos = require('../index.js')
 const path = require('path')
 const fs = require('fs')
 var filePath = path.join(__dirname, '../somedir')
+chai.use(deepEqualInAnyOrder);
 
-describe('File System', function() {
-
-  // My Function Does NOT need to Create Files
-  // My Function will loop through directory to find TODO files
-
-  // Read Directory And Check If Folders Or Files exists
-  // If Folder Exist, Loop Internally and Check if folder or file
-  // If File, read file and check TODO, append File Path To External Array
+describe('Find Todos!', () => {
 
   it ('should return 3 folders', () => {
-    let result
     findtodos(filePath, (files) => {
-      const array = ['somedir1', 'somedir2', 'somedir3']
-      expect(files).to.deep.equal(array)
+      const array = [
+        '/Users/lanbau/code/interview/findtodos/somedir1',
+        '/Users/lanbau/code/interview/findtodos/somedir2',
+        '/Users/lanbau/code/interview/findtodos/somedir3',
+
+      ]
+      // expect(files).to.deep.equalInAnyOrder(array)
+      console.log(files)
     })
   })
-  
+
 
 })
