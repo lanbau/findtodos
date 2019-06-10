9768b7a20232df98499b367dae6486b1eb569d09
irvin lin <irvinln@gmail.com>
2019-06-10 16:34:57 +0800

    find all js files at root

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var random = require('random-name')
-var fs = require('fs')
+var fs = require('fs'),
+    path = require('path'),
+    filePath = path.join(__dirname, '');
 
-const name = random.first().toLowerCase() + '.js'
 
-fs.writeFile(name, 'Yo MAMA', () => {
-    console.log('file created')
+fs.readdir(filePath, (err, files) => {
+  files.forEach(file => {
+    if (file.includes('.js') && !file.includes('.json')) {
+      console.log(file)
+    }
+  })
 })
+
+
+// const name = random.first().toLowerCase() + '.js'
+//
+// fs.writeFile(name, 'Yo MAMA', () => {
+//     console.log('file created')
+// })
