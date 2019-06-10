639b156471f76f4d2e897650e9a23389516abdb8
irvin lin <irvinln@gmail.com>
2019-06-10 16:39:57 +0800

    Create a folder

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 var random = require('random-name')
 var fs = require('fs'),
     path = require('path'),
-    filePath = path.join(__dirname, '');
+    filePath = path.join(__dirname, '')
+
+var dir = './tmp'
+
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir)
+}
 
 
-fs.readdir(filePath, (err, files) => {
-  files.forEach(file => {
-    if (file.includes('.js') && !file.includes('.json')) {
-      fs.readFile(file, 'utf8', (err, contents) => {
-        console.log(contents)
-      })
-    }
-  })
-})
+// fs.readdir(filePath, (err, files) => {
+//   files.forEach(file => {
+//     if (file.includes('.js') && !file.includes('.json')) {
+//       fs.readFile(file, 'utf8', (err, contents) => {
+//         console.log(contents)
+//       })
+//     }
+//   })
+// })
 
 
 // const name = random.first().toLowerCase() + '.js'
